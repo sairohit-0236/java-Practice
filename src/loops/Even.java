0e0b64882a777e8ea678ad06152937b1f65e5726
daro <daro@localhost>
2026-04-10 07:30:00 +0530

    bkn

---
+++ src/loops/Even.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Even {
+	public static void evenn() {
+		for(int i=1;i<=20;i++) {
+			if (i%2 !=0) {
+			System.out.println(i);
+			}
+			}
+	}
+
+	public static void main(String[] args) {
+		evenn();
+
+	}
+
+}
